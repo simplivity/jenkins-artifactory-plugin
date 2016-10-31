7a29a9e8c277a3b6458338ce120d4de5128674b8
Alex Volanis <alexander.volanis@simplivity.com>
2016-10-31 13:41:36 -0400

    Make Artifactory plugin be the last one in the BuildWrappers list.
    In order to guarantee the EnvInject plugin is before this plugin
    in the sorted order of the BuildWrappers the @Extension annotation
    was changed to use ordinal -2000. The EnvInject plugin was also
    modified to use ordinal -1 just so it is the first in the group
    od negative ordinals.
    
    (cherry picked from commit 88bfc36c9f608733793e102754badb55c7366e1b)

--- src/main/java/org/jfrog/hudson/ivy/ArtifactoryIvyConfigurator.java
+++ src/main/java/org/jfrog/hudson/ivy/ArtifactoryIvyConfigurator.java
@@ -325,7 +325,7 @@ public class ArtifactoryIvyConfigurator extends AntIvyBuildWrapper implements De
         return (DescriptorImpl) super.getDescriptor();
     }
 
-    @Extension(optional = true)
+    @Extension(optional = true, ordinal = -2000)
     public static class DescriptorImpl extends AbstractBuildWrapperDescriptor {
         private static final String DISPLAY_NAME = "Publish to Artifactory";
         private static final String CONFIG_PREFIX = "ivy";
